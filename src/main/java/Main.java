78f4f47794a8bebfaee953af7d2b4fba60fa8809
EnesRecep <enesrecepcinar@gmail.com>
2018-12-23 22:49:59 +0300

    seralizeDataAdded

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
+import Model.*;
+import Utils.PacketHandler;
 import Utils.PortHandler;
+import Utils.Serializer;
 import communication.Communication;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.SocketException;
+import java.util.Arrays;
 
 /**
  * Created by Enes Recep on 22.12.2018.
@@ -11,6 +17,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        PacketType packet = new ACKPacket();
+        String rr = "handshakehandshake";
+        DatagramPacket p[] = packet.createPacket(rr,"192.168.0.55",5555);
+
+        Packet pp = new Packet(p[0]);
+
+
+        System.out.println(p[0].getLength());
+        try {
+            Object e = Serializer.deserialize(Arrays.copyOfRange(p[0].getData(),72, 158));
+            System.out.println(e.toString());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+
+
+/*
         PortHandler portHandler = new PortHandler();
         System.out.println(portHandler.getMyPorts()[0]);
 
@@ -23,10 +48,12 @@ public class Main {
         String str = "We will rock you";
         int a = 5;
 
-        communication.send(a,"192.168.0.55");
+        Object o = communication.communicationWait();
+        System.out.println(o.toString());
+        //communication.send(a,"192.168.0.55");
 
 
-
+*/
     }
 
 
--- src/main/java/Utils/Serializer.java
+++ src/main/java/Utils/Serializer.java
@@ -20,8 +20,20 @@ public class Serializer {
         }
     }
 
+    public static byte[] serialize2(Object obj) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(out);
+        os.writeObject(obj);
+        return out.toByteArray();
+    }
+    public static Object deserialize2(byte[] data) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream in = new ByteArrayInputStream(data);
+        ObjectInputStream is = new ObjectInputStream(in);
+        return is.readObject();
+    }
 
-    public static byte[] serialize2(Object obj){
+
+    public static byte[] serialize3(Object obj){
         byte[] yourBytes = null;
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ObjectOutput out = null;
@@ -43,7 +55,7 @@ public class Serializer {
         return yourBytes;
     }
 
-    public static Object deserialize2(byte[] bytes){
+    public static Object deserialize3(byte[] bytes){
         Object object = null;
         ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
         ObjectInput in = null;
